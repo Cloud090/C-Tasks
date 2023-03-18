e85160ca8e75b8fe0e8044c28f0584f5a308ab50
cloud090 <james.brown@brownbrothers.net.au>
2023-03-18 11:05:02 +1100

    Mod2 error fixed

--- Tasks/Class/Mod2/1/Mod2.cs
+++ Tasks/Class/Mod2/1/Mod2.cs
@@ -11,10 +11,10 @@ public class Height //setting class as Cylinder for program
         string value; // storing value for program restart y/n
             do // Running program through do so that program can be restarted
             {
-                int[] array = new int[5] {1, 2, 3, 4, 5};
+                int[] array = new int[5] { 1, 2, 3, 4, 5 };  // Creating an array with 1-5 stored in it & specifying 5 objects in the array
 
-                for (int i = 0; i < array.length(array[i])) {
-                    Console.WriteLine(array[i]);
+                for (int i = 0; i < array.Length; i++) { // Running the loop until the end of the array is met & increasing the value each time the loop runs.
+                    Console.WriteLine(array[i]); // outputting the array value
                 }
 
                 Console.Write("Do you want to do another calculation?(y/n):");
